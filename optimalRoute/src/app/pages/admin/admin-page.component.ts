1df74ab4e8790b30b3c90c3e8eade69e73f7630a
nikita <nikita>
2024-12-13 19:07:48 +0400

    adding context menu

--- optimalRoute/src/app/pages/admin/admin-page.component.ts
+++ optimalRoute/src/app/pages/admin/admin-page.component.ts
@@ -1,17 +1,43 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../header/header.component';
 import Konva from 'konva';
-import {ClassOptimalRoute} from "../../classJSON/ClassOptimalRoure"
-
+import { ClassOptimalRoute } from '../../classJSON/ClassOptimalRoure';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-admin-page',
     standalone: true,
-    imports: [HeaderComponent],
+    imports: [HeaderComponent, CommonModule],
     templateUrl: './admin-page.component.html',
     styleUrl: './admin-page.component.scss',
 })
 export class AdminPageComponent {
+    isContextMenuVisible = false; // Показывать ли меню
+    contextMenuPosition = { x: 0, y: 0 }; // Координаты меню
+
+    openContextMenu(event: MouseEvent): void {
+        event.preventDefault(); // Отключаем стандартное меню браузера
+        this.isContextMenuVisible = true;
+        this.contextMenuPosition = {
+            x: event.clientX, // Горизонтальная позиция мыши
+            y: event.clientY, // Вертикальная позиция мыши
+        };
+    }
+
+    closeContextMenu(): void {
+        this.isContextMenuVisible = false;
+    }
+
+    action1(): void {
+        console.log('Действие 1 выполнено');
+        this.closeContextMenu();
+    }
+
+    action2(): void {
+        console.log('Действие 2 выполнено');
+        this.closeContextMenu();
+    }
+
     [x: string]: any;
     isLeftPanelOpen = true;
     isRightPanelOpen = true;
@@ -27,9 +53,9 @@ export class AdminPageComponent {
 
     private stage: Konva.Stage = {} as any;
     private layer: Konva.Layer = {} as any;
-    
+
     ngOnInit(): void {
-        this.createStage(window.innerWidth / 100 * 56);
+        this.createStage((window.innerWidth / 100) * 56);
     }
 
     //Метод для правой панели
@@ -39,7 +65,6 @@ export class AdminPageComponent {
 
         let tempGridSize = this.gridSize;
         this.drawScaleCanvas(tempGridSize);
-        
     }
 
     //Метод для левой панели
@@ -53,10 +78,13 @@ export class AdminPageComponent {
 
     private gridDrowSize(): void {
         if (this.isRightPanelOpen == true && this.isLeftPanelOpen == true) {
-            this.createStage(window.innerWidth / 100 * 56);
-        } else if (this.isRightPanelOpen == true || this.isLeftPanelOpen == true) {
-            this.createStage(window.innerWidth / 100 * 75);
-        } else this.createStage(window.innerWidth / 100 * 94);
+            this.createStage((window.innerWidth / 100) * 56);
+        } else if (
+            this.isRightPanelOpen == true ||
+            this.isLeftPanelOpen == true
+        ) {
+            this.createStage((window.innerWidth / 100) * 75);
+        } else this.createStage((window.innerWidth / 100) * 94);
     }
 
     private createStage(wight: number): void {
@@ -64,7 +92,7 @@ export class AdminPageComponent {
         this.stage = new Konva.Stage({
             container: 'container', // ID контейнера
             width: wight,
-            height: window.innerHeight / 100 * 91,
+            height: (window.innerHeight / 100) * 91,
         });
 
         // Создаём слой
@@ -101,7 +129,7 @@ export class AdminPageComponent {
 
     addCrossroad(): void {
         if (this.crossroadList.length == 30) {
-            alert("Больше 30 нельзя");
+            alert('Больше 30 нельзя');
             return;
         }
         this.isCrossroadAdd = true;
@@ -112,122 +140,143 @@ export class AdminPageComponent {
     eventClickConvas(X: number, Y: number): void {
         X = Math.round(X / this.gridSize) * this.gridSize;
         Y = Math.round(Y / this.gridSize) * this.gridSize;
-        if (X==0) X += this.gridSize;
-        else if (X==this.stage.width()) X-=this.gridSize;
+        if (X == 0) X += this.gridSize;
+        else if (X == this.stage.width()) X -= this.gridSize;
 
-        if (Y==0) Y += this.gridSize;
-        else if (Y ==this.stage.height()) Y-=this.gridSize;
-        
+        if (Y == 0) Y += this.gridSize;
+        else if (Y == this.stage.height()) Y -= this.gridSize;
+
         if (this.isCrossroadAdd == true) {
             for (let i = 0; i < this.crossroadList.length; i++) {
-                if (this.crossroadList[i].X == X && this.crossroadList[i].Y == Y) {
+                if (
+                    this.crossroadList[i].X == X &&
+                    this.crossroadList[i].Y == Y
+                ) {
                     return;
                 }
             }
             const circle = new Konva.Circle({
                 x: X,
                 y: Y,
-                radius: this.gridSize * 2 / 5,
+                radius: (this.gridSize * 2) / 5,
                 stroke: '#000',
                 full: '#fff',
                 //draggable: true, // Включаем перетаскивание
             });
-    
+
             this.layer.add(circle);
-    
+
             // Обновляем слой
             this.layer.draw();
 
             this.isCrossroadAdd = false;
 
-            let crossroad  = new ClassOptimalRoute.Crossroad();
+            let crossroad = new ClassOptimalRoute.Crossroad();
             crossroad.X = X;
             crossroad.Y = Y;
             this.crossroadList.push(crossroad);
             const jsonCrossroad: string = JSON.stringify(this.crossroadList);
             console.log(jsonCrossroad);
-        }
-        else if (this.isRoadAdd == true && this.indexCrossroad1 >= 0) {
+        } else if (this.isRoadAdd == true && this.indexCrossroad1 >= 0) {
             let i = 0;
             while (i < this.crossroadList.length) {
-                if (this.crossroadList[i].X == X && this.crossroadList[i].Y == Y) {
+                if (
+                    this.crossroadList[i].X == X &&
+                    this.crossroadList[i].Y == Y
+                ) {
                     break;
                 }
                 i++;
             }
             if (i == this.crossroadList.length) {
-                alert("Лох, тут не перекрестка");
+                alert('Лох, тут не перекрестка');
                 return;
             } else if (i == this.indexCrossroad1) {
-                alert("Лох, ты куда прогон добавляешь");
+                alert('Лох, ты куда прогон добавляешь');
                 return;
             }
 
             let j = 0;
             let countIndex = 1;
             while (j < this.roadList.length) {
-                if (this.roadList[j].Crossroad1 == i || this.roadList[j].Crossroad2 == i) {
+                if (
+                    this.roadList[j].Crossroad1 == i ||
+                    this.roadList[j].Crossroad2 == i
+                ) {
                     countIndex++;
                 }
                 if (countIndex > 4) {
-                    alert("Лох, больше 4 нельзя");
+                    alert('Лох, больше 4 нельзя');
                     return;
                 }
                 j++;
             }
 
             for (let y = 0; y < this.roadList.length; y++) {
-                if (this.roadList[y].Crossroad1 == this.indexCrossroad1 && this.roadList[y].Crossroad2 == i ||
-                    this.roadList[y].Crossroad2 == this.indexCrossroad1 && this.roadList[y].Crossroad1 == i
+                if (
+                    (this.roadList[y].Crossroad1 == this.indexCrossroad1 &&
+                        this.roadList[y].Crossroad2 == i) ||
+                    (this.roadList[y].Crossroad2 == this.indexCrossroad1 &&
+                        this.roadList[y].Crossroad1 == i)
                 ) {
-                    alert("Нельзя добавить прогон!!!");
+                    alert('Нельзя добавить прогон!!!');
                     this.isRoadAdd = false;
                     return;
-                } 
+                }
             }
 
             const line = new Konva.Line({
-                points: [this.crossroadList[this.indexCrossroad1].X, this.crossroadList[this.indexCrossroad1].Y, X, Y],
+                points: [
+                    this.crossroadList[this.indexCrossroad1].X,
+                    this.crossroadList[this.indexCrossroad1].Y,
+                    X,
+                    Y,
+                ],
                 stroke: '#000',
                 strokeWidth: 1,
-            })
-    
+            });
+
             this.layer.add(line);
-    
+
             // Обновляем слой
             this.layer.draw();
 
             this.isRoadAdd = false;
 
-            let road  = new ClassOptimalRoute.Road();
+            let road = new ClassOptimalRoute.Road();
             road.Crossroad1 = this.indexCrossroad1;
             road.Crossroad2 = i;
             this.roadList.push(road);
             const jsonRoad: string = JSON.stringify(this.roadList);
             console.log(jsonRoad);
-        }
-        else if (this.isRoadAdd == true) {
+        } else if (this.isRoadAdd == true) {
             let i = 0;
             while (i < this.crossroadList.length) {
-                if (this.crossroadList[i].X == X && this.crossroadList[i].Y == Y) {
+                if (
+                    this.crossroadList[i].X == X &&
+                    this.crossroadList[i].Y == Y
+                ) {
                     break;
                 }
                 i++;
             }
 
             if (this.crossroadList.length == i) {
-                alert("Лох, тут не перекрестка");
+                alert('Лох, тут не перекрестка');
                 return;
             }
 
             let j = 0;
             let countIndex = 1;
             while (j < this.roadList.length) {
-                if (this.roadList[j].Crossroad1 == i || this.roadList[j].Crossroad2 == i) {
+                if (
+                    this.roadList[j].Crossroad1 == i ||
+                    this.roadList[j].Crossroad2 == i
+                ) {
                     countIndex++;
                 }
                 if (countIndex > 4) {
-                    alert("Лох, больше 4 нельзя");
+                    alert('Лох, больше 4 нельзя');
                     return;
                 }
                 j++;
@@ -238,7 +287,7 @@ export class AdminPageComponent {
 
     addRoad(): void {
         if (this.crossroadList.length == 60) {
-            alert("Больше 60 нельзя");
+            alert('Больше 60 нельзя');
             return;
         }
         this.isCrossroadAdd = false;
@@ -253,25 +302,28 @@ export class AdminPageComponent {
         this.gridDrowSize();
 
         this.drawScaleCanvas(tempGridSize);
-
     }
 
     private drawScaleCanvas(tempGridSize: number): void {
         for (let i = 0; i < this.crossroadList.length; i++) {
-            this.crossroadList[i].X = Math.round(this.crossroadList[i].X / tempGridSize) * this.gridSize;
-            this.crossroadList[i].Y = Math.round(this.crossroadList[i].Y / tempGridSize) * this.gridSize;
+            this.crossroadList[i].X =
+                Math.round(this.crossroadList[i].X / tempGridSize) *
+                this.gridSize;
+            this.crossroadList[i].Y =
+                Math.round(this.crossroadList[i].Y / tempGridSize) *
+                this.gridSize;
 
             const circle = new Konva.Circle({
                 x: this.crossroadList[i].X,
                 y: this.crossroadList[i].Y,
-                radius: this.gridSize * 2 / 5,
+                radius: (this.gridSize * 2) / 5,
                 stroke: '#000',
                 full: '#fff',
                 //draggable: true, // Включаем перетаскивание
             });
-    
+
             this.layer.add(circle);
-    
+
             // Обновляем слой
             this.layer.draw();
         }
@@ -279,27 +331,25 @@ export class AdminPageComponent {
         for (let i = 0; i < this.roadList.length; i++) {
             const line = new Konva.Line({
                 points: [
-                        this.crossroadList[this.roadList[i].Crossroad1].X, 
-                        this.crossroadList[this.roadList[i].Crossroad1].Y, 
-                        this.crossroadList[this.roadList[i].Crossroad2].X, 
-                        this.crossroadList[this.roadList[i].Crossroad2].Y,  
-                        ],
+                    this.crossroadList[this.roadList[i].Crossroad1].X,
+                    this.crossroadList[this.roadList[i].Crossroad1].Y,
+                    this.crossroadList[this.roadList[i].Crossroad2].X,
+                    this.crossroadList[this.roadList[i].Crossroad2].Y,
+                ],
                 stroke: '#000',
                 strokeWidth: 1,
-            })
-    
+            });
+
             this.layer.add(line);
-    
+
             // Обновляем слой
             this.layer.draw();
         }
     }
 
     contextMenu(X: number, Y: number) {
-        alert("Тут должно быть контекстное меню!!!" + X + " ," + Y);
+        alert('Тут должно быть контекстное меню!!!' + X + ' ,' + Y);
     }
 
-    checkLimitRoadForCrossroad() {
-        
-    }
+    checkLimitRoadForCrossroad() {}
 }
