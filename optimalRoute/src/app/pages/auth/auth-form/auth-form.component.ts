0e63391189caf3405de5a5d9bcd347d1ff050260
nikita <nikita>
2024-12-29 23:05:01 +0400

    add passwrod visibility change

--- optimalRoute/src/app/pages/auth/auth-form/auth-form.component.ts
+++ optimalRoute/src/app/pages/auth/auth-form/auth-form.component.ts
@@ -1,15 +1,17 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { isRequiredError } from '../../admin/admin-db/forms/validators';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-auth-form',
     standalone: true,
-    imports: [ReactiveFormsModule],
+    imports: [ReactiveFormsModule, CommonModule],
     templateUrl: './auth-form.component.html',
     styleUrl: './auth-form.component.scss',
 })
 export class AuthFormComponent {
+    isPasswordVisible = false;
     @Input() btnText = '';
     @Input() authFormGroup!: FormGroup<{
         login: FormControl<string>;
@@ -18,6 +20,10 @@ export class AuthFormComponent {
     @Output() hasButtonPressed = new EventEmitter<boolean>;
 
     constructor() {}
+
+    changePasswordVisibility() {
+        this.isPasswordVisible = !this.isPasswordVisible;
+    }
 
     click() {
         this.hasButtonPressed.emit(true);
