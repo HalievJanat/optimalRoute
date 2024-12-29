f935329d82ea41f9ea8189a6d43add4645fdf187
nikita <nikita>
2024-12-29 13:37:37 +0400

    add login validation

--- optimalRoute/src/app/pages/auth/auth-form/auth-form.component.ts
+++ optimalRoute/src/app/pages/auth/auth-form/auth-form.component.ts
@@ -1,19 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, inject, Input } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
+import { isRequiredError, stringRangeValidator } from '../../admin/admin-db/forms/validators';
 
 @Component({
     selector: 'app-auth-form',
     standalone: true,
-    imports: [],
+    imports: [ReactiveFormsModule],
     templateUrl: './auth-form.component.html',
     styleUrl: './auth-form.component.scss',
 })
 export class AuthFormComponent {
     @Input() btnText = '';
 
+    private fb = inject(FormBuilder);
+
+    authFormGroup = this.fb.group({
+        login: ['', [Validators.required, stringRangeValidator(12, 4)]],
+        password: ['', [Validators.required, stringRangeValidator(12, 4)]],
+    });
+
     constructor(private router: Router) {}
 
     click() {
         this.router.navigateByUrl('admin-page');
     }
+
+    isRequiredAuthError(form: FormGroup) {
+        return isRequiredError(form);
+    }
+
 }
